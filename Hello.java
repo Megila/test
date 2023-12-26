fc8463c72aa472ae7ac42045f09d47f3c043e80a
Urabe <urabe@example.com>
2023-12-26 17:35:58 +0300

    Изменения в feature-2

--- Hello.java
+++ Hello.java
@@ -1,1 +1,10 @@
-public class Hello { public static void main(String[] args) { System.out.println("B"); System.out.println("Hello, world!"); } }
+public class Hello {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("B");
+=======
+        System.out.println("A");
+>>>>>>> 98ca696f78ded80b2426172af1168b6f37e0db09
+        System.out.println("Hello, world!2");
+    }
+}
